9c6fabd94f620794ebe3b7504451548498362ec9
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-23 17:56:36 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -1,4 +1,3 @@
-import os
 import mlflow
 import mlflow.sklearn
 import dagshub
@@ -8,9 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-assert os.getenv("DAGSHUB_TOKEN"), "DAGSHUB_TOKEN is not set"
-dagshub.auth.add_app_token(os.environ["DAGSHUB_TOKEN"])
-
+# Init DagsHub FIRST
 dagshub.init(
     repo_owner="auliahanasopia",
     repo_name="Workflow-CI",
@@ -24,12 +21,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestClassifier(random_state=42)
-model.fit(X_train, y_train)
+with mlflow.start_run(run_name="rf-ci"):
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
 
-preds = model.predict(X_test)
-acc = accuracy_score(y_test, preds)
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
 
-mlflow.log_param("model", "RandomForest")
-mlflow.log_metric("accuracy", acc)
-mlflow.sklearn.log_model(model, "model")
+    mlflow.log_param("model", "RandomForest")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+
+
