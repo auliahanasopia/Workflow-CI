2a5902c3cdb8796b03fdcf5ffb902a83124fee95
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-23 21:04:22 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -1,36 +1,77 @@
+import warnings
 import mlflow
 import mlflow.sklearn
 import dagshub
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+warnings.filterwarnings("ignore")
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 dagshub.init(
     repo_owner="auliahanasopia",
     repo_name="Workflow-CI",
-    mlflow=True
+    mlflow=False  
 )
 
 mlflow.set_experiment("CI-MLflow-DagsHub")
 
-X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+mlflow.sklearn.autolog(
+    log_input_examples=True,
+    log_model_signatures=True
 )
 
-with mlflow.start_run(run_name="rf-ci", nested=True) as run:
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+X, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+with mlflow.start_run(run_name="rf-ci") as run:
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42,
+        n_jobs=-1
+    )
+
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
     acc = accuracy_score(y_test, preds)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, artifact_path="model")
+    mlflow.log_metric("accuracy_manual", acc)
 
-    # Cetak RUN ID agar bisa dibaca GitHub Actions (opsional)
-    print(f"RUN_ID_OUTPUT:{run.info.run_id}")
+    cm = confusion_matrix(y_test, preds)
+
+    plt.figure(figsize=(6, 4))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+
+    cm_path = "confusion_matrix.png"
+    plt.savefig(cm_path)
+    plt.close()
+
+    mlflow.log_artifact(cm_path)
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+
+    print(f"RUN_ID:{run.info.run_id}")
+
 
 
