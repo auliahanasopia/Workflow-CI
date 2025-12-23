d7d469e23312331180c04448cc959e9438d3ce2d
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-23 17:04:31 +0700

    Add files via upload

---
+++ modelling.py
@@ -0,0 +1,34 @@
+import os
+import mlflow
+import mlflow.sklearn
+import dagshub
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Init DagsHub (NON-interaktif, token via ENV: DAGSHUB_TOKEN)
+dagshub.init(
+    repo_owner="auliahanasopia",
+    repo_name="Workflow-CI",
+    mlflow=True
+)
+
+mlflow.set_experiment("CI-MLflow-DagsHub")
+
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    mlflow.log_param("model", "RandomForest")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
