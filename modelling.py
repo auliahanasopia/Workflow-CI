2f8a6921a53e5196016d7c5e1ba8d91d07cd3e36
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-23 18:35:03 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -1,13 +1,12 @@
 import mlflow
 import mlflow.sklearn
 import dagshub
-
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Init DagsHub FIRST
+# Inisialisasi DagsHub
 dagshub.init(
     repo_owner="auliahanasopia",
     repo_name="Workflow-CI",
@@ -17,19 +16,18 @@ dagshub.init(
 mlflow.set_experiment("CI-MLflow-DagsHub")
 
 X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-with mlflow.start_run(run_name="rf-ci"):
-    model = RandomForestClassifier()
+# Gunakan nested=True agar tidak error saat dipanggil lewat 'mlflow run'
+with mlflow.start_run(run_name="rf-ci", nested=True) as run:
+    model = RandomForestClassifier(n_estimators=100)
     model.fit(X_train, y_train)
-
-    preds = model.predict(X_test)
-    acc = accuracy_score(y_test, preds)
-
-    mlflow.log_param("model", "RandomForest")
+    
+    acc = accuracy_score(y_test, model.predict(X_test))
+    
     mlflow.log_metric("accuracy", acc)
+    # Log model ke DagsHub (ini akan menghasilkan model URI)
     mlflow.sklearn.log_model(model, "model")
-
-
+    
+    # Cetak Run ID agar bisa ditangkap oleh GitHub Actions
+    print(f"RUN_ID_OUPUT:{run.info.run_id}")
