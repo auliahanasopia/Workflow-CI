9ce5a4820a9de2f4282c1c1785b7fc9bd9ccb378
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-23 22:57:07 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -9,12 +9,6 @@ from sklearn.ensemble import RandomForestClassifier
 assert os.getenv("DAGSHUB_TOKEN"), "DAGSHUB_TOKEN is not set"
 dagshub.auth.add_app_token(os.environ["DAGSHUB_TOKEN"])
 
-dagshub.init(
-    repo_owner="auliahanasopia",
-    repo_name="Workflow-CI",
-    mlflow=True
-)
-
 mlflow.set_experiment("CI-MLflow-Project")
 
 data = pd.read_csv("studentmat_preprocessing.csv")
@@ -27,11 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestClassifier(
-    n_estimators=100,
-    random_state=42
-)
-
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 accuracy = model.score(X_test, y_test)
@@ -44,3 +34,4 @@ mlflow.sklearn.log_model(
     artifact_path="model",
     input_example=X_train.iloc[:5]
 )
+
