215b5cf2f6c4062c6b9cb491ba83776c352ac47a
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-24 07:56:04 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -4,8 +4,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-mlflow.set_experiment("CI-MLflow-Project")
-
 data = pd.read_csv("studentmat_preprocessing.csv")
 
 X = data.drop("target", axis=1)
@@ -15,12 +13,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
 model.fit(X_train, y_train)
 
 accuracy = model.score(X_test, y_test)
 
-# logging LANGSUNG
 mlflow.log_param("n_estimators", 100)
 mlflow.log_metric("accuracy", accuracy)
 mlflow.sklearn.log_model(model, "model")
