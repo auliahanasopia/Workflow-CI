20167fdf22af48ebacd5eaff4aa9432e46a09704
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-24 07:32:47 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -9,24 +9,26 @@ from sklearn.ensemble import RandomForestClassifier
 dagshub.auth.add_app_token(os.environ["DAGSHUB_TOKEN"])
 dagshub.init(
     repo_owner="auliahanasopia",
-    repo_name="Workflow-CI",
+    repo_name="student-performance-mlops",
     mlflow=True
 )
 
 mlflow.set_experiment("CI-MLflow-Project")
 
-with mlflow.start_run():
-    data = pd.read_csv("studentmat_preprocessing.csv")
-    X = data.drop("target", axis=1)
-    y = data["target"]
+data = pd.read_csv("studentmat_preprocessing.csv")
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+X = data.drop("target", axis=1)
+y = data["target"]
 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-    acc = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+accuracy = model.score(X_test, y_test)
+
+mlflow.log_param("n_estimators", 100)
+mlflow.log_metric("accuracy", accuracy)
+mlflow.sklearn.log_model(model, "model")
