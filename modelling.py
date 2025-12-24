c8c2b74081cde7efe31a6ebe606a2f71c2504558
auliahanasopia <aulia.sophie03@gmail.com>
2025-12-24 08:41:10 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -4,31 +4,33 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-data = pd.read_csv("studentmat_preprocessing.csv")
+mlflow.set_experiment("CI-MLflow-Project")
 
-X = data.drop("target", axis=1)
-y = data["target"]
+with mlflow.start_run() as run:
+    run_id = run.info.run_id
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    data = pd.read_csv("studentmat_preprocessing.csv")
 
-model = RandomForestClassifier(
-    n_estimators=100,
-    random_state=42
-)
-model.fit(X_train, y_train)
+    X = data.drop("target", axis=1)
+    y = data["target"]
 
-accuracy = model.score(X_test, y_test)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-mlflow.log_param("n_estimators", 100)
-mlflow.log_metric("accuracy", accuracy)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
 
-mlflow.sklearn.log_model(
-    model,
-    artifact_path="model",
-    registered_model_name="student-performance-model"
-)
+    accuracy = model.score(X_test, y_test)
+
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.sklearn.log_model(model, "model")
+
+    with open("run_id.txt", "w") as f:
+        f.write(run_id)
+
+
 
 
 
